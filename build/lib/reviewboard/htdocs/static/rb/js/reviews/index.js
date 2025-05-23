0512105e41c9a5bba9d0467d1f763e9691146373
sijinhui <62741773+sijinhui@users.noreply.github.com>
2025-05-23 18:00:17 +0800

    1

--- build/lib/reviewboard/htdocs/static/rb/js/reviews/index.js
+++ build/lib/reviewboard/htdocs/static/rb/js/reviews/index.js
@@ -2216,8 +2216,9 @@
       async markShipIt(idElement = '') {
         const pendingReview = this.get('pendingReview');
         await pendingReview.ready();
+        const temp_bodyTop = idElement === '' ? 'Ship It!' : `Ship It!|${idElement}`;
         pendingReview.set({
-          bodyTop: `Ship It!|${idElement}`,
+          bodyTop: temp_bodyTop,
           shipIt: true
         });
         await pendingReview.publish();
